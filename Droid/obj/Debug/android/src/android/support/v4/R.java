0261295f44c24f0d70e42fa7c257d3897754b1ad
Jeeva Nadarajah <jeeva.ganeshan@gmail.com>
2015-05-02 22:37:48 -0400

    changed the androidimages

--- Droid/obj/Debug/android/src/android/support/v4/R.java
+++ Droid/obj/Debug/android/src/android/support/v4/R.java
@@ -11,15 +11,25 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static int accounts=0x7f020000;
-        public static int contacts=0x7f020001;
-        public static int icon=0x7f020002;
-        public static int leads=0x7f020003;
-        public static int opportunities=0x7f020004;
-        public static int settings=0x7f020005;
+        public static int about=0x7f020000;
+        public static int accounts=0x7f020001;
+        public static int contacts=0x7f020002;
+        public static int hurtlogger80=0x7f020003;
+        public static int icon=0x7f020004;
+        public static int leads=0x7f020005;
+        public static int opportunities=0x7f020006;
+        public static int plus=0x7f020007;
+        public static int plus24=0x7f020008;
+        public static int profile=0x7f020009;
+        public static int recent=0x7f02000a;
+        public static int settings=0x7f02000b;
+        public static int view=0x7f02000c;
+    }
+    public static final class raw {
+        public static int hurtloggersqlite=0x7f030000;
     }
     public static final class string {
-        public static int ApplicationName=0x7f030001;
-        public static int Hello=0x7f030000;
+        public static int ApplicationName=0x7f040001;
+        public static int Hello=0x7f040000;
     }
 }
