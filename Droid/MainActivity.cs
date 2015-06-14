8f77d274a89d77a5eac7027513ebeaf28c4c8ea0
Jeeva Nadarajah <jeeva.ganeshan@gmail.com>
2015-06-14 19:18:07 -0400

    removed unused icons

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,16 +1,11 @@
-﻿using System;
-
+﻿
 using Android.App;
-using Android.Content;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace HurtLogger.Droid
 {
-	[Activity (Label = "HurtLogger.Droid", Icon = "@drawable/hurtlogger80", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity (Label = "HurtLogger.Droid", Icon = "@drawable/ic_launcher", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
 		protected override void OnCreate (Bundle bundle)
--- Droid/Resources/Resource.designer.cs
+++ Droid/Resources/Resource.designer.cs
@@ -59,40 +59,34 @@ namespace HurtLogger.Droid
 			public const int hamburger = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int hurtlogger80 = 2130837508;
+			public const int ic_launcher = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int ic_launcher = 2130837509;
+			public const int icon = 2130837509;
 			
 			// aapt resource value: 0x7f020006
-			public const int icon = 2130837510;
+			public const int leads = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int leads = 2130837511;
+			public const int opportunities = 2130837511;
 			
 			// aapt resource value: 0x7f020008
-			public const int opportunities = 2130837512;
+			public const int plus = 2130837512;
 			
 			// aapt resource value: 0x7f020009
-			public const int plus = 2130837513;
+			public const int plus24 = 2130837513;
 			
 			// aapt resource value: 0x7f02000a
-			public const int plus24 = 2130837514;
+			public const int profile = 2130837514;
 			
 			// aapt resource value: 0x7f02000b
-			public const int profile = 2130837515;
+			public const int recent = 2130837515;
 			
 			// aapt resource value: 0x7f02000c
-			public const int recent = 2130837516;
+			public const int users = 2130837516;
 			
 			// aapt resource value: 0x7f02000d
-			public const int settings = 2130837517;
-			
-			// aapt resource value: 0x7f02000e
-			public const int users = 2130837518;
-			
-			// aapt resource value: 0x7f02000f
-			public const int view = 2130837519;
+			public const int view = 2130837517;
 			
 			static Drawable()
 			{
